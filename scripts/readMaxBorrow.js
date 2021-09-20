a19b7785471969aedc04a525324894681a549fad
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2021-09-20 20:46:04 +0800

    add contract folders and files

---
+++ scripts/readMaxBorrow.js
@@ -0,0 +1,10 @@
+const MyDeFiProject = artifacts.require('MyDeFiProject.sol');
+
+const cBatAddress = '0xebf1a11532b93a529b5bc942b4baa98647913002'; 
+
+module.exports = async done => {
+  const myDeFiProject = await MyDeFiProject.deployed();
+  const maxBorrow = await myDeFiProject.getMaxBorrow(cBatAddress);
+  console.log(`Max Bat Balance: ${maxBorrow}`);
+  done();
+}
---
+++ scripts/redeemDai.js
@@ -0,0 +1,9 @@
+const MyDeFiProject = artifacts.require('MyDefiProject.sol');
+
+const cDaiAddress = '0x6d7f0754ffeb405d23c51ce938289d4835be3b14';
+
+module.exports = async done => {
+    const myDefiProject = await MyDeFiProject.deployed();
+    await myDefiProject.redeem(cDaiAddress, 0.9632 * Math.pow(10,8));
+    done();
+};
